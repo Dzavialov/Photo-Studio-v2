8f8015bf409a97857689905e42a06d692d13e4bc
Dzavialov <dimazua1409@gmail.com>
2023-10-18 15:59:04 +0300

    Created token service

---
+++ api/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using api.DTOs;
+using api.Entities;
+using api.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
+
+        public AccountController(UserManager<User> userManager, ITokenService tokenService, IMapper mapper)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+            _mapper = mapper;
+        }
+
+        public async Task<ActionResult<UserDto>> Register(User user)
+        {
+            if (await UserExists(user.UserName)) return BadRequest("Username is already taken.");
+
+            
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _userManager.Users.AnyAsync(u => u.UserName == username.ToLower());
+        }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,11 @@
 using api.Data;
+using api.Entities;
+using api.Interfaces;
+using api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace api
 {
@@ -12,10 +18,32 @@ namespace api
             // Add services to the container.
 
             builder.Services.AddControllers();
+            
             builder.Services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+            
+            builder.Services.AddIdentityCore<User>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+            }).AddEntityFrameworkStores<DataContext>();
+            
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding
+                        .UTF8.GetBytes(builder.Configuration["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            builder.Services.AddScoped<ITokenService, TokenService>();
 
             var app = builder.Build();
 
