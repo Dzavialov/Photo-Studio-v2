aa4bf08e9eac13cf321acb39cd647ef20781b072
Dzavialov <dimazua1409@gmail.com>
2023-10-19 16:40:05 +0300

    Created account service

---
+++ client/src/app/_services/account-service.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { User } from '../_models/User';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountServiceService {
+  baseUrl = environment.apiUrl
+  private currentUserSource = new BehaviorSubject<User | any>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user){
+          this.setCurrentUser(user);
+        }
+      })
+    )
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if(user){
+          this.setCurrentUser(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    user.role = '';
+    const role = this.getDecodedToken(user.token).role;
+    user.role = role;
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]))
+  }
+}
